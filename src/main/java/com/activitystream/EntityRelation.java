79390973e6034d62b99b3b3d6e0529b038e9a8c4
Petar Shomov <petar@activitystream.com>
2015-09-08 18:17:37 +0000

    adding some more properties on the relationship

--- src/main/java/com/activitystream/EntityRelation.java
+++ src/main/java/com/activitystream/EntityRelation.java
@@ -2,12 +2,28 @@ package com.activitystream;
 
 import org.json.simple.JSONObject;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
+import java.util.SimpleTimeZone;
+import java.util.TimeZone;
 
 public class EntityRelation {
     private LinkType linkType;
     private Entity entity;
+    private Map props;
+    private Date startDate;
+    private Date endDate;
+    private Double weight;
+    private static DateFormat isoDateFormatter;
+
+    static {
+        TimeZone tz = TimeZone.getTimeZone("UTC");
+        isoDateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        isoDateFormatter.setTimeZone(tz);
+    }
+
 
     public EntityRelation link(LinkType linkType, Entity entity) {
         this.linkType = linkType;
@@ -16,15 +32,19 @@ public class EntityRelation {
     }
 
     public EntityRelation properties(Map props){
+        this.props = props;
         return this;
     }
-    public EntityRelation activeFrom(Date startDate){
+    public EntityRelation validFrom(Date startDate){
+        this.startDate = startDate;
         return this;
     }
     public EntityRelation activeUntil(Date endDate){
+        this.endDate = endDate;
         return this;
     }
-    public EntityRelation weight(Integer weight){
+    public EntityRelation weight(Double weight){
+        this.weight = weight;
         return this;
     }
 
@@ -34,6 +54,10 @@ public class EntityRelation {
         JSONObject obj = new JSONObject();
 
         obj.put("type", linkType.toJson());
+        if (weight != null) obj.put("weight", weight);
+        if (startDate != null) obj.put("valid_from", isoDateFormatter.format(startDate));
+        if (endDate != null) obj.put("active_until", endDate);
+        if (props != null) obj.put("properties", props);
         entity.addToObject(obj);
 
         return obj;
