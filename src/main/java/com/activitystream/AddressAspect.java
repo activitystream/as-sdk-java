4e4978f55d74df8f57e88e2ea80a68d0680be345
Petar Shomov <petar@activitystream.com>
2015-09-08 16:55:05 +0000

    refactoring for an easier aspect construction

--- src/main/java/com/activitystream/AddressAspect.java
+++ src/main/java/com/activitystream/AddressAspect.java
@@ -2,42 +2,69 @@ package com.activitystream;
 
 import org.json.simple.JSONObject;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddressAspect implements Aspect {
-    private String addr;
-    private String city;
-    private String line2;
-    private String countryCode;
-    private String zipCode;
+
+    private Map<String, AspectProperty> aspectPropertyMap = new HashMap<>();
+
+    public AddressAspect() {
+        aspectPropertyMap.put("address.address", new AspectProperty(true));
+        aspectPropertyMap.put("address.address2", new AspectProperty(false));
+        aspectPropertyMap.put("address.city", new AspectProperty(false));
+        aspectPropertyMap.put("address.country_code", new AspectProperty(false));
+        aspectPropertyMap.put("address.zip_code", new AspectProperty(false));
+    }
 
     public AddressAspect streetAndNumber(String addr){
-        this.addr = addr;
+        aspectPropertyMap.get("address.address").value = addr;
         return this;
     }
     public AddressAspect line2(String line2){
-        this.line2 = line2;
+        aspectPropertyMap.get("address.address2").value = line2;
         return this;
     }
     public AddressAspect city(String city){
-        this.city = city;
+        aspectPropertyMap.get("address.city").value = city;
         return this;
     }
     public AddressAspect countryCode(String countryCode){
-        this.countryCode = countryCode;
+        aspectPropertyMap.get("address.country_code").value = countryCode;
         return this;
     }
     public AddressAspect zipCode(String zipCode) {
-        this.zipCode = zipCode;
+        aspectPropertyMap.get("address.zip_code").value = zipCode;
         return this;
     }
 
     @Override
-    public void addToObject(JSONObject jsonObject2) {
-        JSONObject addressObj = new JSONObject();
-        if (addr != null) addressObj.put("address", addr);
-        if (line2 != null) addressObj.put("address2", line2);
-        if (city != null) addressObj.put("city", city);
-        if (countryCode != null) addressObj.put("country_code", countryCode);
-        if (zipCode != null) addressObj.put("zip_code", zipCode);
-        jsonObject2.put("address", addressObj);
+    public void addToObject(JSONObject aspectJson) {
+        for (Map.Entry<String, AspectProperty> aspect : aspectPropertyMap.entrySet()) {
+            if (aspect.getValue().required && aspect.getValue().value == null) throw new RuntimeException("Property "+aspect.getValue()+" is required ");
+            String[] levels = aspect.getKey().split("\\.");
+            String aspectPropertyKey = levels[levels.length-1];
+            levels = Arrays.copyOf(levels, levels.length - 1);
+            JSONObject propertyParent = aspectJson;
+            for(String level : levels){
+                JSONObject drill = (JSONObject) propertyParent.get(level);
+                if (drill == null) {
+                    drill = new JSONObject();
+                    propertyParent.put(level, drill);
+                }
+                propertyParent = drill;
+            }
+            propertyParent.put(aspectPropertyKey, aspect.getValue().value);
+        }
+    }
+
+    protected class AspectProperty {
+        public String value;
+        public boolean required;
+
+        public AspectProperty(boolean required) {
+            this.required = required;
+        }
     }
 }
