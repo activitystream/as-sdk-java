a5b720e792215af02338c63a9edadf69544fea18
Petar Shomov <petar@activitystream.com>
2015-10-01 15:22:04 +0000

    a better order of attributes for the parameters

--- src/main/java/com/activitystream/underware/Factories.java
+++ src/main/java/com/activitystream/underware/Factories.java
@@ -1,8 +1,9 @@
 package com.activitystream.underware;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class Factories {
-    public static <K,V> Map<K,V> getMap() {return new TreeMap<K,V>();}
+    public static <K,V> Map<K,V> getMap() {return new TreeMap<K,V>(Collections.reverseOrder());}
 }
