66f1567ec7de7121211384de9ea05c7243ec0fcc
Petar Shomov <petar@activitystream.com>
2015-10-12 15:50:42 +0000

    adding some new relations

--- src/main/java/com/activitystream/Predefined.java
+++ src/main/java/com/activitystream/Predefined.java
@@ -18,6 +18,10 @@ public class Predefined {
     public static EntityRelationType PROXY_FOR = new EntityRelationType("PROXY_FOR");
     public static EntityRelationType BELONGS_TO = new EntityRelationType("BELONGS_TO");
     public static EntityRelationType SUPPLIED_BY = new EntityRelationType("SUPPLIED_BY");
+    public static EntityRelationType RELATED_TO = new EntityRelationType("RELATED_TO");
+    public static EntityRelationType ASSOCIATED_WITH = new EntityRelationType("ASSOCIATED_WITH");
+    public static EntityRelationType ON_BEHALF_OF = new EntityRelationType("ON_BEHALF_OF");
+    public static EntityRelationType HAS_RELATIONS_TO = new EntityRelationType("HAS_RELATIONS_TO");
     public static EntityRelationType LOCATED_AT = new EntityRelationType("LOCATED_AT");
     public static EntityRelationType FEATURED = new EntityRelationType("FEATURED");
     public static EntityRelationType HOSTED_AT = new EntityRelationType("HOSTED_AT");
