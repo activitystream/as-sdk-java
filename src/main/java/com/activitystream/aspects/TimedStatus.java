d3128a56c5705419adf4868204347f0a44f0cb88
Petar Shomov <petar@activitystream.com>
2015-10-22 13:33:45 +0000

    adding due

--- src/main/java/com/activitystream/aspects/TimedStatus.java
+++ src/main/java/com/activitystream/aspects/TimedStatus.java
@@ -1,11 +1,12 @@
 package com.activitystream.aspects;
 
 public enum TimedStatus {
-    VALID;
+    VALID, DUE;
 
     @Override
     public String toString() {
         if (this == VALID) return "valid";
+        if (this == DUE) return "due";
         throw new RuntimeException("unhandled action of timed action");
     }
 }
