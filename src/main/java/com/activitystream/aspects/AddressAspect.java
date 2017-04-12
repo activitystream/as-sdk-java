e48b1a1b8517dab4bc7d39f97343a38907c3ed1d
Guðmundur Claxton <gudmundur@activitystream.com>
2017-04-12 12:29:34 +0000

    SS-188

--- src/main/java/com/activitystream/aspects/AddressAspect.java
+++ src/main/java/com/activitystream/aspects/AddressAspect.java
@@ -6,9 +6,13 @@ public class AddressAspect extends AspectBase {
         aspectPropertyMap.put("address.address", new AspectProperty(IsRequired.False));
         aspectPropertyMap.put("address.address2", new AspectProperty(IsRequired.False));
         aspectPropertyMap.put("address.city", new AspectProperty(IsRequired.False));
+        aspectPropertyMap.put("address.state_code", new AspectProperty(IsRequired.False));
         aspectPropertyMap.put("address.state", new AspectProperty(IsRequired.False));
         aspectPropertyMap.put("address.country_code", new AspectProperty(IsRequired.False));
+        aspectPropertyMap.put("address.country", new AspectProperty(IsRequired.False));
         aspectPropertyMap.put("address.zip_code", new AspectProperty(IsRequired.False));
+        aspectPropertyMap.put("address.region", new AspectProperty(IsRequired.False));
+        aspectPropertyMap.put("address.sub_region", new AspectProperty(IsRequired.False));
     }
 
     public AddressAspect streetAndNumber(String addr) {
@@ -40,13 +44,33 @@ public class AddressAspect extends AspectBase {
         return this;
     }
 
+    public AddressAspect country(String country) {
+        aspectPropertyMap.get("address.country").value = country;
+        return this;
+    }
+
     public AddressAspect zipCode(String zipCode) {
         aspectPropertyMap.get("address.zip_code").value = zipCode;
         return this;
     }
 
+    public AddressAspect stateCode(String stateCode) {
+        aspectPropertyMap.get("address.state_code").value = stateCode;
+        return this;
+    }
+
     public AddressAspect state(String state) {
         aspectPropertyMap.get("address.state").value = state;
+        return this;
+    }
+
+    public AddressAspect region(String region) {
+        aspectPropertyMap.get("address.region").value = region;
+        return this;
+    }
+
+    public AddressAspect subRegion(String subRegion) {
+        aspectPropertyMap.get("address.sub_region").value = subRegion;
         return this;
     }
 }
