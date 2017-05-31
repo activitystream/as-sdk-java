16d111f5ebc6124ff525eb029c144d1b95c4fbbd
Gudmundur Orn Johannsson <gudmundur.orn@gmail.com>
2017-05-31 11:33:34 +0000

    Add build scripts for GoCD

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -8,6 +8,8 @@ set -euxo pipefail
 
 ACCESS=$1 # public or private
 REVISION=$2
+shift
+shift
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOT="${DIR}/../.."
@@ -18,8 +20,7 @@ export AWS_SECRET_ACCESS_KEY
 cd $ROOT
 
 if [ "$ACCESS" == "public" ]; then
-    sed -i "s|<artifactId>sdk-internal</artifactId>|<artifactId>sdk</artifactId>|" pom.xml
+    sed -i "s|<artifactId>sdk-internal</artifactId>|<artifactId>sdk</artifactId>|" $ROOT/pom.xml
 fi
 mvn -s $ROOT/scripts/build/settings.xml versions:set -DnewVersion=$REVISION
-shift
 mvn -s $ROOT/scripts/build/settings.xml $@ # Expected arguments: "clean package" "clean deploy"
